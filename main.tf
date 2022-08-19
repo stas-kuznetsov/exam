42605e714ccdd5f5901de29e3b019828f2d5e514
stas-kuznetsov <89393832+stas-kuznetsov@users.noreply.github.com>
2022-08-19 15:41:17 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,57 @@
+
+resource "azurerm_resource_group" "example" {
+  name     = "Exam"
+  location = "France Central"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "project-vnet"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.1.0.0/24"]
+}
+
+resource "azurerm_network_interface" "ProdNIC" {
+  name                = "ProdNIC"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "ProdT" {
+  name                = "ProdT"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B2s"
+  admin_username      = "stas"
+  network_interface_ids = [azurerm_network_interface.ProdNIC.id]
+
+  admin_ssh_key {
+    username   = "stas"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
